9e0fdd6c230b9ff193c6075cff4d5e351c910d66
Matt Osborne <MattJOzzy@gmail.com>
2023-03-02 16:56:31 -0600

    created database and tables with initial seed data

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (department_name)
+VALUES  ("business"),
+        ("computer science"),
+        ("engineering");
+
+INSERT INTO roles (title, salary)
+VALUES  ("business professor", 75000),
+        ("computer science professor", 75000),
+        ("engineering professor", 75000);
+
+INSERT INTO names (first_name, last_name)
+VALUES  ("Brad", "Pitt"),
+        ("Matt", "Damon"),
+        ("Shia", "LeBeouf");
